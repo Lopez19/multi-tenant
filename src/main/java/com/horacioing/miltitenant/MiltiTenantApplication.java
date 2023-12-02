abb897af1139681d0370c35f601a7c2ff10eeebb
Lopez19 <horaciolopm_2001@hotmail.com>
2023-12-02 12:40:16 -0500

    first commit

---
+++ src/main/java/com/horacioing/miltitenant/MiltiTenantApplication.java
@@ -0,0 +1,13 @@
+package com.horacioing.miltitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiltiTenantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiltiTenantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/horacioing/miltitenant/MiltiTenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horacioing.miltitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiltiTenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
